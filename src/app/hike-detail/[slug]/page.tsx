e24e38da0ebd949e1813ba79f5892952124030ff
unknown <titarevdeniz@yandex.ru>
2025-02-19 14:37:17 +0300

    fix

--- src/app/hike-detail/[slug]/page.tsx
+++ src/app/hike-detail/[slug]/page.tsx
@@ -1,8 +1,5 @@
 import ContactUs from "@/components/ContactUs/ContactUs";
-import UpcomingHikes from "@/components/UpcomingHikes/UpcomingHikes";
 import ProductPage from "@/customPages/ProductPage/ProductPage";
-import OAuth from "oauth-1.0a";
-import crypto from "crypto";
 import { wordpressUrlWC } from "@/app/globalUrl";
 import { notFound } from "next/navigation";
 
@@ -15,39 +12,16 @@ async function getProductBySlug({
   params: Promise<{ slug: string }>;
 }) {
   const slug = (await params).slug;
-  const API_URL = `${wordpressUrlWC}/products?slug=${slug}`;
+  const API_URL = `${wordpressUrlWC}/products?slug=${slug}&consumer_key=${CONSUMER_KEY}&consumer_secret=${CONSUMER_SECRET}`;
 
-  const oauth = new OAuth({
-    consumer: { key: CONSUMER_KEY, secret: CONSUMER_SECRET },
-    signature_method: "HMAC-SHA1",
-    hash_function(base_string, key) {
-      return crypto
-        .createHmac("sha1", key)
-        .update(base_string)
-        .digest("base64");
-    },
-  });
-  // Запрос всех товаров (WooCommerce не поддерживает slug напрямую)
-  const request_data = {
-    url: `${API_URL}`, // Запрашиваем 100 товаров (оптимально для малого каталога)
-    method: "GET",
-  };
-
-  const authHeader = oauth.toHeader(oauth.authorize(request_data));
-
-  const res = await fetch(request_data.url, {
-    method: "GET",
-    headers: {
-      Authorization: authHeader.Authorization,
-    },
-  });
+  const res = await fetch(API_URL, { method: "GET" });
 
   if (!res.ok) {
-    notFound(); // Перенаправляет на 404
+    notFound();
   }
-  const products = await res.json();
 
-  const product = products.find((p: any) => p.slug === slug);
+  const products = await res.json();
+  const product = products.length ? products[0] : null;
 
   if (!product) {
     throw new Error(`Товар с slug "${slug}" не найден`);
@@ -64,7 +38,7 @@ export default async function HikesSinglePage({
   const product = await getProductBySlug({ params });
 
   return (
-    <main className="padding-top-content ">
+    <main className="padding-top-content">
       <ProductPage
         id={product.id}
         title={product.name}
@@ -83,7 +57,6 @@ export default async function HikesSinglePage({
       <section className="main-container">
         <ContactUs />
       </section>
-      {/* <UpcomingHikes /> */}
     </main>
   );
 }
